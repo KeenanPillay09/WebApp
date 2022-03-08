e89f3f3cc6d9142c39080b1e92f8ffc66c60f7d1
KeenanPillay09 <54399643+KeenanPillay09@users.noreply.github.com>
2022-03-08 09:13:03 +0200

    Initial Solution Setup

---
+++ WebApp/WebApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp.WebUI.Startup))]
+namespace WebApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
